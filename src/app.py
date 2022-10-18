a3a786ae75c0efb8243aeeb501c9411fa19add88
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2022-10-18 14:04:18 +0200

    docker compose changed

---
+++ src/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request
+import json
+from elastic_index import Index
+
+
+app = Flask(__name__)
+
+config = {
+    "url" : "localhost",
+    "port" : "9200",
+    "doc_type" : "manuscript"
+}
+
+index = Index(config)
+
+
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+@app.route("/")
+def hello_world():
+    retStruc = {"app": "eCodices NL service", "version": "0.1"}
+    return json.dumps(retStruc)
+
+@app.route("/facet", methods=['GET'])
+def get_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    ret_struc = index.get_facet(facet + ".keyword", amount)
+    return json.dumps(ret_struc)
+
+# @app.route("/nested_facet", methods=['GET'])
+# def get_nested_facet():
+#     facet = request.args.get("name")
+#     amount = request.args.get("amount")
+#     path = request.args.get("path")
+#     ret_struc = index.get_nested_facet(path, facet + ".keyword", amount)
+#     return json.dumps(ret_struc)
+
+@app.route("/filter-facet", methods=['GET'])
+def get_filter_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    facet_filter = request.args.get("filter")
+    ret_struc = index.get_filter_facet(facet + ".keyword", amount, facet_filter)
+    return json.dumps(ret_struc)
+
+@app.route("/browse", methods=['POST'])
+def browse():
+    struc = request.get_json()
+    ret_struc = index.browse(struc["page"], struc["page_length"], struc["sortorder"] + ".keyword", struc["searchvalues"])
+    return json.dumps(ret_struc)
+
+@app.route("/item", methods=['GET'])
+def manuscript():
+    id = request.args.get('id')
+    manuscript = index.item(id)
+    return json.dumps(manuscript)
+
+@app.route("/get_collection", methods=["POST"])
+def get_collection():
+    data = request.get_json()
+    print(data)
+    collection_items = index.get_collection_items(data["collection"])
+    return json.dumps(collection_items);
+
+
+
+
+
+#Start main program
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ src/elastic_index.py
@@ -0,0 +1,139 @@
+from elasticsearch import Elasticsearch
+import math
+
+
+class Index:
+    def __init__(self, config):
+        self.config = config
+        # self.es = Elasticsearch(hosts=[{"host": "ecodices_es"}], retry_on_timeout=True)
+        self.client = Elasticsearch()
+
+    def no_case(self, str_in):
+        str = str_in.strip()
+        ret_str = ""
+        if (str != ""):
+            for char in str:
+                ret_str = ret_str + "[" + char.upper() + char.lower() + "]"
+        return ret_str + ".*"
+
+    def get_facet(self, field, amount):
+        ret_array = []
+        response = self.client.search(
+            index="diplo",
+            body=
+            {
+                "size": 0,
+                "aggs": {
+                    "names": {
+                        "terms": {
+                            "field": field,
+                            "size": amount,
+                            "order": {
+                                "_term": "asc"
+                            }
+                        }
+                    }
+                }
+            }
+        )
+        for hits in response["aggregations"]["names"]["buckets"]:
+            buffer = {"key": hits["key"], "doc_count": hits["doc_count"]}
+            ret_array.append(buffer)
+        return ret_array
+
+
+
+    def get_filter_facet(self, field, amount, facet_filter):
+        print(facet_filter)
+        ret_array = []
+        response = self.client.search(
+            index="diplo",
+            body=
+            {
+                "query": {
+                    "regexp": {
+                        field: self.no_case(facet_filter)
+                    }
+                },
+                "size": 0,
+                "aggs": {
+                    "names": {
+                        "terms": {
+                            "field": field,
+                            "size": amount,
+                            "order": {
+                                "_count": "desc"
+                            }
+                        }
+                    }
+                }
+            }
+        )
+        for hits in response["aggregations"]["names"]["buckets"]:
+            buffer = {"key": hits["key"], "doc_count": hits["doc_count"]}
+            ret_array.append(buffer)
+        return ret_array
+
+    def item(self, id):
+        response = self.client.search(
+            index="diplo",
+            body={"query": {
+                "terms": {
+                    "_id": [id]
+                }
+            },
+                "_source": ["titel", "naam", "locatie", "organisatie", "onderwerp", "rol"]
+            }
+        )
+        if response["hits"]["total"]["value"] == 1:
+            return response["hits"]["hits"][0]["_source"]
+        else:
+            return []
+
+    def browse(self, page, length, orderFieldName, searchvalues):
+        int_page = int(page)
+        start = (int_page - 1) * length
+        matches = []
+
+        if searchvalues == []:
+            response = self.client.search(
+                index="diplo",
+                body={"query": {
+                    "match_all": {}},
+                    "size": length,
+                    "from": start,
+                    "_source": ["titel", "naam", "locatie", "organisatie", "onderwerp", "rol"],
+                    "sort": [
+                        {orderFieldName: {"order": "asc"}}
+                    ]
+                }
+            )
+        else:
+            for item in searchvalues:
+                for value in item["values"]:
+                    if item["field"] == "FREE_TEXT":
+                        matches.append({"multi_match": {"query": value, "fields": ["*"]}})
+                    else:
+                        matches.append({"match": {item["field"] + ".keyword": value}})
+            response = self.client.search(
+                index="diplo",
+                body={"query": {
+                    "bool": {
+                        "must": matches
+                    }},
+                    "size": length,
+                    "from": start,
+                    "_source": ["titel", "naam", "locatie", "organisatie", "onderwerp", "rol"],
+                    "sort": [
+                        {orderFieldName: {"order": "asc"}}
+                    ]
+                }
+            )
+
+        ret_array = {"amount": response["hits"]["total"]["value"],
+                     "pages": math.ceil(response["hits"]["total"]["value"] / length), "items": []}
+        for item in response["hits"]["hits"]:
+            tmp_arr = item["_source"]
+            tmp_arr["_id"] = item["_id"]
+            ret_array["items"].append(tmp_arr)
+        return ret_array
